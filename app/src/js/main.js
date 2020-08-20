00f5e8ced41ce2eac51067f4a26292cda3c32906
unknown <>
2020-08-20 12:44:53 +0100

    final js touches

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -1,34 +1,35 @@
-//Access element controls.
-let colorCode = document.querySelector(".colorCode");
-let button = document.querySelector("input");
-let jsBackground = document.querySelector(".js");
+// Access element controls.
+const colorCode = document.querySelector('.colorCode');
+const button = document.querySelector('input');
+const jsBackground = document.querySelector('.js');
 
-button.addEventListener("click", function () {
-  changeBackgroundColor();
-})
+// array for random code generation.
+const arrayBox = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
 
-let arrayBox = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
-
+// generates random number for array index.
 function randGen() {
   return Math.floor(Math.random() * arrayBox.length);
 }
 
+// runs loop to generate hexcode form array.
 function getHex() {
-  let hexCode = "";
+  let hexCode = '';
+  // eslint-disable-next-line no-plusplus
   for (let i = 0; i < 6; i++) {
     hexCode += arrayBox[randGen()];
   }
   return `#${hexCode}`;
 }
 
+// displays hexcode
+// and changes element background color to hexcode
 function changeBackgroundColor() {
-
-  let hexCode = getHex();
+  const hexCode = getHex();
   colorCode.textContent = hexCode;
 
   jsBackground.style.backgroundColor = hexCode;
-
-  
-  // console.log(beforeColor);
 }
 
+button.addEventListener('click', () => {
+  changeBackgroundColor();
+});
